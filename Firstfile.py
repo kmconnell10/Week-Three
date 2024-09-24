6690236663c9abfef808f2d9211534755a60af67
Karen Connell <kmconnell10@gmail.com>
2024-09-24 10:01:46 -0400

    Update Firstfile.py
    We made two identical outputs one using the sep command and the other using the + operator and comas in quotaions.

---
+++ Firstfile.py
@@ -0,0 +1,11 @@
+# "Class of CS 1-2
+# Period 3
+# 9/24/2024"
+
+firstName= "Jayanth"
+lastName= "Gordon"
+age = 15
+
+print(firstName,lastName,str(age),sep=",");
+print(firstName+","+lastName+","+str(age));
+
